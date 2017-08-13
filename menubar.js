81d074cd7037e31a3b4303ec0af8a1557e72540f
Kristinn Júlíusson <kristinnj13@ru.is>
2017-08-13 21:17:33 +0000

    add menubar electron app

--- menubar.js
+++ menubar.js
@@ -1,13 +1,29 @@
 const menubar = require('menubar');
 const path = require('path');
+const BrowserWindow = require('electron').BrowserWindow;
 
 const mb = menubar({
   tooltip: 'Wow such toolbar',
-  index: path.join('file://', __dirname, 'index.html'),
+  index: 'http://localhost:3000', //path.join('file://', __dirname, 'index.html'),
 });
 
-mb.on('ready', function ready() {
-  console.log('app is ready');
-  title = 'Your app is ready';
-  // your app code here
-});
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://localhost:3000');
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+mb.on('ready', createWindow);
